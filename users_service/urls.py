602def61a82784d2300a88dcc9a5d198c4b25ee1
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-07-03 00:22:46 -0300

    Feat: Add home page.

--- users_service/urls.py
+++ users_service/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
     path('users/', include('users.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
